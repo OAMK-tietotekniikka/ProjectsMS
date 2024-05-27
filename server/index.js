b6d8976f93747a2996be4fb68c7120907b468a5e
Bao <tranxbao@gmail.com>
2024-05-27 20:26:26 +0300

    production branch

--- server/index.js
+++ server/index.js
@@ -7,11 +7,10 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'https://cop-client-cop-ms.2.rahtiapp.fi/',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
-  }));
-
+}));
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World' });
