70a454fb11b6736488b4673f098d0d872704daf2
Bao <tranxbao@gmail.com>
2024-06-12 01:56:46 +0300

    students table modified

--- dbinit/init.sql
+++ dbinit/init.sql
@@ -8,8 +8,8 @@ id          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
 first_name  VARCHAR(255) DEFAULT NULL,
 last_name   VARCHAR(255) DEFAULT NULL,
 email       VARCHAR(255) DEFAULT NULL,
-phone       VARCHAR(255) DEFAULT NULL,
-status      VARCHAR(30) DEFAULT NULL,
+class_code  VARCHAR(255) DEFAULT NULL,
+password    VARCHAR(30) DEFAULT NULL,
 created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (id),
 CONSTRAINT UQ_Students_Email UNIQUE (email)
@@ -18,7 +18,7 @@ CONSTRAINT UQ_Students_Email UNIQUE (email)
 
 -- Insert student data
 INSERT INTO students (first_name, last_name, email, phone, status)
-VALUES ('John', 'Doe', 'johndoe@email.com', '123456789', 'admitted');
+VALUES ('John', 'Doe', 'johndoe@email.com', 'din21sp', '1234');
 
 INSERT INTO students (first_name, last_name, email, phone, status)
-VALUES ('Jane', 'Doe', 'janedoe@email.com', '987654321', 'admitted');
+VALUES ('Jane', 'Doe', 'janedoe@email.com', 'din20sp', '1235');
